88ea98b7a20a4c41d28fa3c5dfc41ff45f580414
Augusto Bunge <abunge7@gmail.com>
2022-04-27 18:54:25 +1000

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import logo from './pelotaMundial.png'
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Branding">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Prode Mundial 2022</h1>
+        </div>
+        <NavBar />
       </header>
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+function NavBar() {
+
+    return (
+        <nav className="App-navbar">
+            <li><button>Fixture</button></li>
+            <li><button>Resultados</button></li>
+            <li><button>Premios</button></li>
+        </nav>
+    )
+}
+
+export default NavBar
